9ec26d186fdc491ea288b7cc9b172b382730e388
Felipe Silva <felipeconceicaodasilva@hotmail.com>
2025-09-02 23:36:47 -0300

    Utilitarios criado

--- Program.cs
+++ Program.cs
@@ -1,23 +1,12 @@
 ﻿namespace CRUD_App
 {
-    class Program
+    class Interfaces
     {
-        public static void MensagemColorida(string mensagem, ConsoleColor cor)
-        {
-            var corAntiga = Console.ForegroundColor;
-            Console.ForegroundColor = cor;
-            Console.WriteLine(mensagem);
-            Console.ForegroundColor = corAntiga;
-        }
-        public static void Tag(string mensagem, ConsoleColor cor)
-        {
-            var corAntiga = Console.ForegroundColor;
-            Console.ForegroundColor = cor;
-            Console.Write($"[{mensagem}]");
-            Console.ForegroundColor = corAntiga;
-        }
+
+
         public static int ExibirMenu()
         {
+            Console.Clear();
             Console.WriteLine("=== GERENCIADOR DE TAREFAS ===");
             Console.WriteLine("1 - Adicionar tarefa");
             Console.WriteLine("2 - Listar tarefas");
@@ -26,15 +15,25 @@
             Console.WriteLine("0 - Sair");
             Console.Write("Escolha uma opção: ");
 
-            string entrada = Console.ReadLine();
+            string entrada = Utilitarios.LerString();
 
             return int.TryParse(entrada, out int opcao) ? opcao : -1;
 
         }
+    }
+    class Program
+    {
+
+
         static void Main(String[] args)
         {
-            Tag("O", ConsoleColor.Green); Console.WriteLine("teste");
-            Tag("X", ConsoleColor.Red); Console.WriteLine("teste2");
+            bool continuar = true;
+            do
+            {
+                string mensagem = Utilitarios.LerString();
+                Console.WriteLine(mensagem);
+            } while (continuar);
+
         }
     }
 }
---
+++ Utilitarios.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_App
+{
+    public static class Utilitarios
+    {
+        public static void MensagemColorida(string mensagem, ConsoleColor cor)
+        {
+            var corAntiga = Console.ForegroundColor;
+            Console.ForegroundColor = cor;
+            Console.WriteLine(mensagem);
+            Console.ForegroundColor = corAntiga;
+        }
+        public static void Tag(string mensagem, ConsoleColor cor)
+        {
+            var corAntiga = Console.ForegroundColor;
+            Console.ForegroundColor = cor;
+            Console.Write($"[{mensagem}]");
+            Console.ForegroundColor = corAntiga;
+        }
+        public static string LerString(string mensagem = "Digite algo")
+        {
+            Console.Write(mensagem + ": ");
+            string mensagemValidada = (Console.ReadLine() ?? "").Trim();
+            if (string.IsNullOrWhiteSpace(mensagemValidada))
+            {
+                MensagemColorida("Entrada inválida. Tente novamente.", ConsoleColor.Yellow);
+                return LerString(mensagem);
+            }
+            return mensagemValidada;
+        }
+    }
+}
